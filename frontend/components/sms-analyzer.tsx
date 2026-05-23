0b287f28a8f478548dda02b38c5345dec68f070a
waisalam <waisalam9523@gmail.com>
2026-05-24 03:11:47 +0530

    changes in fething link

--- frontend/components/sms-analyzer.tsx
+++ frontend/components/sms-analyzer.tsx
@@ -31,7 +31,7 @@ export default function SMSAnalyzer() {
 
     try {
       // Simulate API call to Flask backend 
-  const response = await fetch('http://127.0.0.1:5000/predict',{
+  const response = await fetch('https://spamsmsdetection-r49y.onrender.com/predict',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
